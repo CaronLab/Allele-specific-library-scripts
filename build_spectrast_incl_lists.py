ff78ff1818f9ee3544cd0aebcd6b34097f408be1
Kevin Kovalchik <kkovalchik.chusj@gmail.com>
2021-03-01 09:29:26 -0500

    Update build_spectrast_incl_lists.py

--- build_spectrast_incl_lists.py
+++ build_spectrast_incl_lists.py
@@ -14,6 +14,7 @@ with open(pep_file, 'r') as f:
     header = f.readline().strip().split()
     contents = [x.strip().split() for x in f.readlines()]
 
+label_index = header.index('Label')
 rank_headers = [x for x in header if '_rank' in x]
 rank_indices = [header.index(x) for x in rank_headers]
 allele_by_index = {x: header[x].replace('_rank', '') for x in rank_indices}
@@ -23,5 +24,7 @@ pep_index = header.index('SpectraST_Peptide')
 for allele_index in rank_indices:
     with open(f'{allele_by_index[allele_index]}_inclusion_list.tsv', 'w') as f:
         for line in contents:
+            if line[label_index].lower() != 'target':
+                continue
             if float(line[allele_index]) <= cutoff:
                 f.write(line[pep_index] + '\n')
